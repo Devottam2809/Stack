d3aeb435a1a7b60cb7f8c58bf6940d65afc81510
its_DC <96367023+Devottam2809@users.noreply.github.com>
2023-04-08 19:13:07 +0530

    Create NSL.cpp

---
+++ NSL.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of elements : ";
+    cin>>n;
+
+    vector<int>v;
+    cout<<"Enter the elements of the vector :"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        int data;
+        cin>>data;
+        v.push_back(data);
+    }
+
+    stack<int>s;
+    vector<int>ans;
+    for(int i=n-1;i>0;i--)
+    {
+        if(s.size()==0)
+            ans.push_back(-1);
+        else if(s.top()<v[i])
+            ans.push_back(s.top());
+        else if(s.size()>0 && s.top()>=v[i])
+        {
+            while(s.size()>0 && s.top()>=v[i])
+                s.pop();
+                if(s.size()==0)
+                    ans.push_back(-1);
+                else
+                    ans.push_back(s.top());
+        }
+    s.push(v[i]);
+    }
+    reverse(ans.begin(),ans.end());
+    for(int i=0;i<ans.size();i++)
+        cout<<ans[i]<<" ";
+    return 0;
+}
